553e0047ba30a3afba1c719217decb5afa87679a
Jacob Duncan <jacobduncan00@yahoo.com>
2020-09-01 20:38:16 -0400

    1

---
+++ build/precache-manifest.885c5f7c925336fc2f78b3b26c2a77a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef19ed2d1e4cf11afa54547b0b1336e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac68c5445a3bbca32a87",
+    "url": "/static/css/main.1d49b866.chunk.css"
+  },
+  {
+    "revision": "c9093d785a5ed223877e",
+    "url": "/static/js/2.115e69c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.115e69c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac68c5445a3bbca32a87",
+    "url": "/static/js/main.fd9b2fc5.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
